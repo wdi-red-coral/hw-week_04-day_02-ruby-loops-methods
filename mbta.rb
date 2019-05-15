faacd02118c298e7a5b2509005ca472ae51b8c05
saeedalzahrani <saeedalzahrani103@gmail.com>
2019-05-15 10:11:56 +0300

    not solved yet

---
+++ mbta.rb
@@ -0,0 +1,18 @@
+start_point = gets.to_s
+end_point   = gets.to_s
+total = 0
+line_red.each do |i,index|
+    puts "#{index}" + "#{i}" 
+end
+line_red =  [ 
+    'South Station',
+    'Park Street',
+    'Kendall',
+    'Central',
+    'Harvard',
+    'Porter',
+    'Davis',
+    'Alewife'   
+]
+puts total
+
